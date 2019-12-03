212078d263ee623d2d9a0947f2b29d552fc9399c
Glen <ultrapancake@gmail.com>
2019-12-02 21:26:37 -0700

    added js file to hit post route on click for add employee

---
+++ public/js/add-employee.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#addEmp").click(function() {
+    //grabbing user input from form on add-employee page
+    var newEmployee = {
+      firstName: $("#firstName").val(),
+      lastName: $("#lastName").val(),
+      empID: $("#empID").val(),
+      title: $("#empTitle").val(),
+      markets: $("empMarkets").val()
+    };
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#empID").val("");
+    $("#empTitle").val("");
+    $("#empMarkets").val("");
+    $.ajax({
+      url: "/api/add-employee",
+      method: "POST",
+      data: newEmployee
+    }).then(function(response) {
+      console.log(response);
+      //do something there
+    });
+  });
+});
