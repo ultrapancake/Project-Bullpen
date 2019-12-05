e4076231dcb71a173dfbb46cfa6e5a07973a6731
Glen <ultrapancake@gmail.com>
2019-12-04 17:51:03 -0700

    set up config file for local host to hit with Postman

--- models/projects.js
+++ models/projects.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
-    var Projects = sequelize.define("Projects", {
-        owner: DataTypes.STRING,
-        contractValue: DataTypes.INTEGER,
-        market: DataTypes.STRING,
-        startDate: DataTypes.DATE,
-        finishDate: DataTypes.DATE
-    });
-    return Projects;
+  var Projects = sequelize.define("Projects", {
+    owner: DataTypes.STRING,
+    contractValue: DataTypes.INTEGER,
+    market: DataTypes.STRING,
+    startDate: DataTypes.STRING,
+    finishDate: DataTypes.STRING
+  });
+  return Projects;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,45 +2,46 @@ var db = require("../models");
 var axios = require("axios");
 
 module.exports = function(app) {
-    // Get all examples
-    app.get("/api/examples", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.json(dbExamples);
-        });
+  // Get all examples
+  app.get("/api/examples", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.json(dbExamples);
     });
+  });
 
-    app.get("/api/employees", function(req, res) {
-        db.Employees.findAll({}).then(function(dbPost) {
-            res.json(dbPost);
-        });
+  app.get("/api/employees", function(req, res) {
+    db.Employees.findAll({}).then(function(dbPost) {
+      res.json(dbPost);
     });
+  });
 
-    // Create a new example
-    app.post("/api/examples", function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
-            res.json(dbExample);
-        });
+  // Create a new example
+  app.post("/api/examples", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
     });
+  });
 
-    app.post("/api/add-employee", function(req, res) {
-        db.Employees.create(req.body).then(function(dbPost) {
-            res.json(dbPost);
-        });
+  app.post("/api/add-employee", function(req, res) {
+    db.Employees.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
     });
+  });
 
-    // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.json(dbExample);
-        });
+  // Delete an example by id
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.json(dbExample);
     });
+  });
 
-    app.delete("/api/remove-employee/:id", function(req, res) {
-        db.Employees.destroy({
-            where: { id: req.params.id }
-        }).then(function(dbPost) {
-            res.json(dbPost);
-        });
+  app.delete("/api/remove-employee/:id", function(req, res) {
+    db.Employees.destroy({
+      where: { id: req.params.id }
+    }).then(function(dbPost) {
+      res.json(dbPost);
     });
-
+  });
 };
