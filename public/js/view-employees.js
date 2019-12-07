dc14e07119953016fc6dc61f9a11e85116b2f977
mememoomoo <jackrichardlarsen@gmail.com>
2019-12-07 15:09:57 -0700

    Added Employee javascript + styling

---
+++ public/js/view-employees.js
@@ -0,0 +1,48 @@
+$.ajax({
+  url: "/api/view-employees",
+  type: "GET",
+  crossDomain: true,
+  headers: {
+    Authorization: "Basic " + btoa("_system:SYS"),
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+  }
+}).then(function(response) {
+  for (var i = 0; i < response.length; i++) {
+    var firstName = response[i].firstName;
+    var lastName = response[i].lastName;
+    var employeeMarket = response[i].market;
+    var employeeTitle = response[i].title;
+    var employeeProjects = response[i].projects;
+    var employeeId = response[i].employeeId;
+    
+    let append = `<div class="card mt-4 mr-4 ml-4">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-6">
+          <h1 class="card-title employeeName">${firstName} ${lastName}</h1>
+        </div>
+        <div class="col-6">
+          <h2 class="card-title employeeMarket">${employeeMarket}</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <h3 class="card-title employeeTitle">${employeeTitle}</h3>
+        </div>
+        <div class="col-6">
+          <h4 class="card-title employeeProjects">${employeeProjects}</h4>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <a id="${employeeId}"class="btn btn-primary">More Info</a>
+        </div>
+      </div>
+    </div>
+  </div>`
+
+  $("employee-div").append(append);
+}
+});
